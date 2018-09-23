5c4aa2ec12c56fb5e5d069b6511316492e970800
Longluffy <Longluffy@DESKTOP-C3LJVFU>
2018-09-24 01:27:50 +0700

    untrack unneeded files

--- ViettelAuto/src/main/java/MainRunnable.java
+++ ViettelAuto/src/main/java/MainRunnable.java
@@ -5,15 +5,15 @@ import com.vt.dto.NapTheDTO;
 import com.vt.dto.NapTheMethodEnum;
 
 public class MainRunnable extends Thread {
-	private final static String PATH_TO_EXE_SELENIUM = "C:\\Users\\Admin\\git\\mainRabbitMQClient\\testRabbitMQ\\tool\\phantomjs\\phantomjs.exe";
+	private final static String PATH_TO_EXE_SELENIUM = "C:\\Users\\Longluffy\\Desktop\\tool\\phantomjs\\phantomjs.exe";
 
 	public static void main(String[] args) {
 		Thread threadKeepSession = new Thread() {
 			public void run() {
-				LoginDTO loginDto = new LoginDTO("h004_gftth_longnd193", "020791a@", LoginMethodEnum.ADSL_FTTH_NEXTTV);
-				NapTheDTO naptheDto = new NapTheDTO(NapTheMethodEnum.DIDONG_DCOM, "0961005566", "511380099753328");
+				LoginDTO loginDto = new LoginDTO("t008_gftth_linhnt228", "Yen@123456", LoginMethodEnum.ADSL_FTTH_NEXTTV);
+				NapTheDTO naptheDto = new NapTheDTO(NapTheMethodEnum.DIDONG_DCOM, "0961005566", "117892656195269");
 
-				for (int i = 0; i < 2; i++) {
+				for (int i = 0; i < 1; i++) {
 					System.out.println("Chay lan : " + i);
 					try {
 						Thread.sleep(1000);
